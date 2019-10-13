58b029b10815345a31b67d9f86d94d3accf53c9c
11tools <11tools@github.com>
2019-10-13 13:00:50 +0800

    init fsmc lcd display and fat file system support

---
+++ software/HARDWARE/3.4TFT/LCD.h
@@ -0,0 +1,108 @@
+#include "sys.h"
+
+//屏幕开始时显示方式，注意：当IDelay时显示第一幅画面是逐像素刷新的
+//此时必须手动在刷新结束后加上  LCD_WR_REG(0x0007,0x0173);才能显示
+//当Immediately时没有被注释掉是不需要此过程
+
+/* 选择BANK1-BORSRAM1 连接 TFT，地址范围为0X60000000~0X63FFFFFF
+ * FSMC_A16 接LCD的DC(寄存器/数据选择)脚
+ * 16 bit => FSMC[24:0]对应HADDR[25:1]
+ * 寄存器基地址 = 0X60000000
+ * RAM基地址 = 0X60020000 = 0X60000000+2^16*2 = 0X60000000 + 0X20000 = 0X60020000
+ * 当选择不同的地址线时，地址要重新计算。
+ */
+#define Bank1_LCD_D    ((u32)0x60020000)    //Disp Data ADDR
+#define Bank1_LCD_C    ((u32)0x60000000)	   //Disp Reg ADDR
+
+#define Set_Rst GPIOB->BSRRL = GPIO_Pin_15;
+#define Clr_Rst GPIOB->BSRRH  = GPIO_Pin_15;
+
+#define Lcd_Light_ON   GPIOB->BSRRL = GPIO_Pin_10;
+#define Lcd_Light_OFF  GPIOB->BSRRH  = GPIO_Pin_10;
+
+
+
+
+//Lcd初始化及其低级控制函数
+void Lcd_Configuration(void);
+void DataToWrite(u16 data);
+void Lcd_Initialize(void);
+void LCD_WR_REG(u16 Index,u16 CongfigTemp);
+void Lcd_WR_Start(void);
+//Lcd高级控制函数
+void Lcd_ColorBox(u16 x,u16 y,u16 xLong,u16 yLong,u16 Color);
+void DrawPixel(u16 x, u16 y, u16 Color);
+u16 ReadPixel(u16 x,u8 y);
+void LCD_Fill_Pic(u16 x, u16 y,u16 pic_H, u16 pic_V, const unsigned char* pic);
+void BlockWrite(unsigned int Xstart,unsigned int Xend,unsigned int Ystart,unsigned int Yend);
+char Tiky_Button(char *filename,u16 x,u16 y);
+
+
+/*定义常见颜色*/
+// #define red 0x001f
+// #define blue 0xf800
+// #define green 0x07e0
+// #define black 0x0000
+// #define white 0xffff
+// #define yellow 0x07ff
+// #define orange 0x05bf
+// #define Chocolate4 0x4451
+// #define Grey 0xefbd//灰色
+
+#define White          0xFFFF
+#define Black          0x0000
+#define Blue           0x001F
+#define Blue2          0x051F
+#define Red            0xF800
+#define Magenta        0xF81F
+#define Green          0x07E0
+#define Cyan           0x7FFF
+#define Yellow         0xFFE0
+
+//LCD重要参数集
+typedef struct  
+{										    
+	u16 width;			//LCD 宽度
+	u16 height;			//LCD 高度
+	u16 id;				//LCD ID
+	u8  dir;			//横屏还是竖屏控制：0，竖屏；1，横屏。	
+	u16	 wramcmd;		//开始写gram指令
+	u16  setxcmd;		//设置x坐标指令
+	u16  setycmd;		//设置y坐标指令	 
+}_lcd_dev; 	
+
+//LCD参数
+extern _lcd_dev lcddev;	//管理LCD重要参数
+
+
+#define RED  	  0xf800
+#define GREEN	  0x07e0
+#define BLUE 	  0x001f
+#define WHITE	  0xffff
+#define BLACK	  0x0000
+#define YELLOW  0xFFE0
+#define GRAY0   0xEF7D   	//灰色0 3165 00110 001011 00101
+#define GRAY1   0x8410      	//灰色1      00000 000000 00000
+#define GRAY2   0x4208      	//灰色2  1111111111011111
+
+/////////////////////////////////////用户配置区///////////////////////////////////	 
+//支持横竖屏快速定义切换，支持8/16位模式切换
+#define USE_HORIZONTAL  	0	    //定义是否使用横屏 		0,不使用.  1,使用.
+//使用模拟SPI作为测试
+
+
+
+void LCD_GPIO_Init(void);//初始化IO口
+void LCD_WriteData_16Bit(uint16_t Data);//向液晶屏写一个16位数据
+
+void Lcd_WriteIndex(u8 Index);//写控制器寄存器地址
+void Lcd_WriteData(u8 Data);//  写寄存器数据
+void Lcd_WriteReg(u8 Index,u8 Data);////写寄存器函数
+
+u16 Lcd_ReadReg(u8 LCD_Reg);
+
+void Lcd_Reset(void);
+void Lcd_Init(void);
+void Lcd_Clear(u16 Color);
+void Lcd_SetXY(u16 x,u16 y);
+void Gui_DrawPoint(u16 x,u16 y,u16 Data);
